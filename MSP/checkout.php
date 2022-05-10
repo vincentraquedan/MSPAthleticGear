61531106548b4d447a9fb2fc5f1ec696b6ad597c
SpaceSheep09 <56585187+SpaceSheep09@users.noreply.github.com>
2022-05-10 19:33:46 +0800

    Add files via upload

---
+++ MSP/checkout.php
@@ -0,0 +1,171 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:user_login.php');
+};
+
+if(isset($_POST['order'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $number = $_POST['number'];
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $method = $_POST['method'];
+   $method = filter_var($method, FILTER_SANITIZE_STRING);
+   $address = 'flat no. '. $_POST['flat'] .', '. $_POST['street'] .', '. $_POST['city'] .', '. $_POST['state'] .', '. $_POST['country'] .' - '. $_POST['pin_code'];
+   $address = filter_var($address, FILTER_SANITIZE_STRING);
+   $total_products = $_POST['total_products'];
+   $total_price = $_POST['total_price'];
+
+   $check_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+   $check_cart->execute([$user_id]);
+
+   if($check_cart->rowCount() > 0){
+
+      $insert_order = $conn->prepare("INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price) VALUES(?,?,?,?,?,?,?,?)");
+      $insert_order->execute([$user_id, $name, $number, $email, $method, $address, $total_products, $total_price]);
+
+      $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+      $delete_cart->execute([$user_id]);
+
+      $message[] = 'order placed successfully!';
+   }else{
+      $message[] = 'your cart is empty';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Checkout</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="checkout-orders">
+
+   <form action="" method="POST">
+
+   <h3>Your Orders</h3>
+
+      <div class="display-orders">
+      <?php
+         $grand_total = 0;
+         $cart_items[] = '';
+         $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+         $select_cart->execute([$user_id]);
+         if($select_cart->rowCount() > 0){
+            while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
+               $cart_items[] = $fetch_cart['name'].' ('.$fetch_cart['price'].' x '. $fetch_cart['quantity'].') - ';
+               $total_products = implode($cart_items);
+               $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
+      ?>
+         <p> <?= $fetch_cart['name']; ?> <span>(<?= '₱'.$fetch_cart['price'].' x '. $fetch_cart['quantity']; ?>)</span> </p>
+      <?php
+            }
+         }else{
+            echo '<p class="empty">your cart is empty!</p>';
+         }
+      ?>
+         <input type="hidden" name="total_products" value="<?= $total_products; ?>">
+         <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
+         <div class="grand-total">Grand Total : <span>₱<?= $grand_total; ?></span></div>
+      </div>
+
+      <h3>Order Details</h3>
+
+      <div class="flex">
+         <div class="inputBox">
+            <span>Your Name :</span>
+            <input type="text" name="name" placeholder="Enter Name" class="box" maxlength="30" required>
+         </div>
+         <div class="inputBox">
+            <span>Your Number :</span>
+            <input type="number" name="number" placeholder="Enter Contact Number" class="box" min="0" max="9999999999" onkeypress="if(this.value.length == 10) return false;" required>
+         </div>
+         <div class="inputBox">
+            <span>Your E-mail :</span>
+            <input type="email" name="email" placeholder="Enter E-mail Address" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Payment Method :</span>
+            <select name="method" class="box" required>
+               <option value="COD">Cash on Delivery</option>
+               <option value="GCash">GCash</option>
+               <option value="Paymaya">Paymaya</option>
+               <option value="Bank Transfer">Bank Transfer</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>Address 01 :</span>
+            <input type="text" name="flat" placeholder="Example: Apartment Number" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Address 02 :</span>
+            <input type="text" name="street" placeholder="Example: Street Name" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>City :</span>
+            <input type="text" name="city" placeholder="Example: Makati City" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>State :</span>
+            <input type="text" name="state" placeholder="Example: Metro Manila" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Country :</span>
+            <input type="text" name="country" placeholder="Example: Philippines" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Pin Code: </span>
+            <input type="number" min="0" name="pin_code" placeholder="Example: 123456" min="0" max="999999" onkeypress="if(this.value.length == 6) return false;" class="box" required>
+         </div>
+      </div>
+
+      <input type="submit" name="order" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>" value="place order">
+
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ MSP/components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i>Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i>About Us</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i>Products</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i>Contact Us</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i>Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i>Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i>Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i>Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <a href="tel:+639178169422"><i class="fas fa-phone"></i>0917 816 9422</a>
+         <a href="mailto:michellespatel@yahoo.com"><i class="fas fa-envelope"></i>michellespatel@yahoo.com</a>
+         <a href="https://www.google.com/maps/dir/14.5736274,121.022031/14.96842,120.61088/@14.8111212,120.533517,10z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"><i class="fas fa-map-marker-alt"></i>2003 Guagua, Philippines</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/MSPAthleticGearSublimation"><i class="fab fa-facebook-f"></i>Facebook</a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; <?= date('Y'); ?> MSP Athletic Gear. All Rights Reserved</div>
+
+</footer>
